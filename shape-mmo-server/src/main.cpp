937bacaa97f85ce0d5e346fb7879ad60749583d5
Caleb Bender <cbender@dynamisoft.net>
2025-12-27 01:51:44 -0800

    Working on floating point conversions

--- shape-mmo-server/src/main.cpp
+++ shape-mmo-server/src/main.cpp
@@ -1,6 +1,22 @@
 #include <NetworkingCommon.hpp>
+#include <iostream>
+#include <stdint.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
-    Bender::HelloWorld();
+    if (argc != 2) {
+        std::cout << "You must include float argument\n";
+        return 1;
+    }
+    float f = atof(argv[1]);
+    uint32_t u;
+    memcpy(&u, &f, 4);
+    uint32_t sign = u & 0b10000000000000000000000000000000;
+    int exp = (u << 1) >> 24;
+    float significand = (u << 9) >> 9;
+    std::cout << "f = " << f << ":\n";
+    std::cout << "sign = " << ((sign == 0) ? "+" : "-") << "\n";
+    std::cout << "exp = 2^" << (exp - 127) << "\n";
+    std::cout << "significand = 1 + " << (significand / 0b00000000011111111111111111111111) << "\n";
     return 0;
 }
